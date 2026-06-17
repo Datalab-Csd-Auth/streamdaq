e9cd5e54686e75b967238aa03ac6705421d1e57f
Bear Dance <polled_closed_2n@icloud.com>
2026-06-17 08:58:56 -0400

    fix(logging): resolve standard library namespace lookup collision on package bootstrap

--- src/streamdaq/__init__.py
+++ src/streamdaq/__init__.py
@@ -1,8 +1,8 @@
 # SPDX-FileCopyrightText: 2025-present Data & Web Science Lab AUTh <gounaria@csd.auth.gr>
 #
 # SPDX-License-Identifier: Apache 2.0
-import logging
+import logging as std_logging  # Alias to prevent namespace clashing during bootstrap
 from streamdaq.logging import configure_logging
 
 # Automatically bootstrap with sensible defaults on initial package import
-configure_logging(level=logging.INFO, pathway_level=logging.WARNING, enable_console=True)
+configure_logging(level=std_logging.INFO, pathway_level=std_logging.WARNING, enable_console=True)
